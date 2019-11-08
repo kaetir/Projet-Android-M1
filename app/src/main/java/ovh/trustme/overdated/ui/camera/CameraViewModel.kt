059ab4e22e4fd852d2f820459290ab1be3d3b55b
kaetir <thibault.desprez@isen.yncrea.fr>
2019-11-08 15:28:56 +0100

    le design

--- app/src/main/java/ovh/trustme/overdated/ui/camera/CameraViewModel.kt
+++ app/src/main/java/ovh/trustme/overdated/ui/camera/CameraViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CameraViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Camera Fragment"
+        value = "Lecteur de code barres"
     }
     val text: LiveData<String> = _text
 }
